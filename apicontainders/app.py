545ed6f7cc5f8014ceea8583063248950531fc3c
t3rm!nal <144577613+Terminal127@users.noreply.github.com>
2024-01-27 19:55:18 +0530

    Update app.py

--- apicontainders/app.py
+++ apicontainders/app.py
@@ -3,13 +3,11 @@ from pydantic import BaseModel
 from pymongo import MongoClient
 from typing import List, Optional
 from datetime import datetime
-import os
-
 app = FastAPI()
 
 # MongoDB connection
 try:
-    client = MongoClient(os.environ.get("MONGODB_URI"))
+    client = MongoClient("mongodb://mongodb:27017/imagedb")
     db = client["imagedb"]  # Create a new database named "imagedb"
     image_collection = db["imagecollection"]  # Create a new collection named "imagecollection"
 
@@ -34,29 +32,30 @@ except Exception as e:
     raise
 
 class Image(BaseModel):
-    id: Optional[str]
+    id: str
     url: str
     created_at: Optional[datetime] = None
 
 # Function to get all images from the database
 def get_all_images():
-    return [{"id": str(image["_id"]), "url": image["url"], "created_at": image["created_at"]} for image in image_collection.find()]
-
+    return [{"id": image["id"], "url": image["url"], "created_at": image["created_at"]} for image in image_collection.find()]
 # Function to add a new image to the database
 async def add_image(new_image):
     new_image_data = {
+        "id": new_image.id,
         "url": new_image.url,
         "created_at": new_image.created_at
     }
     result = image_collection.insert_one(new_image_data)  # Await MongoDB operation
-    new_image.id = str(result.inserted_id)
     return new_image
 
-# Function to delete an image by ID
+# Function to delete an ima
+
 def delete_image(image_id):
-    result = image_collection.delete_one({"_id": image_id})
-    if result.deleted_count == 0:
+    result = image_collection.find_one_and_delete({"id": image_id})
+    if result is None:
         raise HTTPException(status_code=404, detail=f"Image with id {image_id} not found")
+
 
 # Get all images
 @app.get("/images", response_model=List[Image])
@@ -73,5 +72,4 @@ async def add_image_endpoint(image: Image):
 @app.delete("/delete_image/{image_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_image_endpoint(image_id: str):
     delete_image(image_id)
-    return None  # Return None as the response for successful deletion
-
+    return (f"deleted image with {image_id}")  # Return None as the response for successful deletion
